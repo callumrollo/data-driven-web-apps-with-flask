1ec85fa36d40f50e2f9926b52480a8a074a0f31e
Callum Rollo <c.rollo@outlook.com>
2020-12-23 11:56:54 +0000

    first db class

---
+++ app/ch09_sqlalchemy/starter/pypi_org/data/packages.py
@@ -0,0 +1,18 @@
+import sqlalchemy as sa
+
+class Package:
+    __tablename__ = 'packages'
+    id = sa.Column(sa.String, primary_key=True)
+    created_date = sa.Column(sa.DateTime)
+    summary = sa.Column(sa.String)
+
+    home_page = sa.Column(sa.String)
+    docs_url = sa.Column(sa.String)
+    package_url = sa.Column(sa.String)
+
+    author_name = sa.Column(sa.String)
+    author_email = sa.Column(sa.String)
+    license = sa.Column(sa.String)
+
+    def __repr__(self):
+        return '<Package {}>'.format(self.id)
