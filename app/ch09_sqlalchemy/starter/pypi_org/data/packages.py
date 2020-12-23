9ab0d963bb4721cd886460c629a4e17bff047776
Callum Rollo <c.rollo@outlook.com>
2020-12-23 12:41:29 +0000

    made db connection

--- app/ch09_sqlalchemy/starter/pypi_org/data/packages.py
+++ app/ch09_sqlalchemy/starter/pypi_org/data/packages.py
@@ -1,6 +1,9 @@
 import sqlalchemy as sa
 
-class Package:
+from pypi_org.data.modelbase import SqlAlchemyBase
+
+
+class Package(SqlAlchemyBase):
     __tablename__ = 'packages'
     id = sa.Column(sa.String, primary_key=True)
     created_date = sa.Column(sa.DateTime)
